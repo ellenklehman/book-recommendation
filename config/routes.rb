b787ef53defdcaabf5fcddaa774f9bac3e19f050
Ellen Lehman <ellenklehman@users.noreply.github.com>
2014-09-12 12:23:30 -0700

    add redirect to user profile page after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   root to: 'books#index'
-  resources :books
+  resources :lists do
+    resources :books
+  end
+  resources :users, only: [:show]
 end
