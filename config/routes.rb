3087f7c44f1646b1b14cef35bc6fbda8e40f750f
Ellen Lehman <ellenklehman@users.noreply.github.com>
2014-09-15 21:37:39 -0700

    WIP render books of a particular list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
-  root to: 'books#index'
+  root to: 'users#show'
 
   resources :users, only: [:show]
   resources :lists
